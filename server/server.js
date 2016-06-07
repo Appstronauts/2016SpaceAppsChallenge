69bbc7807db780ce21a1e73e8a1177c2aa6d1b03
Malek Ascha <malekascha@gmail.com>
2016-06-07 14:53:05 -0700

    added sentry monitoring

--- server/server.js
+++ server/server.js
@@ -3,7 +3,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const raven = require('raven');
 const port = process.env.PORT || 3000;
+
+function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(res.sentry+'\n');
+}
+
+// The request handler must be the first item
+app.use(raven.middleware.express.requestHandler(process.env.SENTRYDSN));
+
+app.use(raven.middleware.express.errorHandler(process.env.SENTRYDSN));
 
 app.use(express.static(__dirname + '/../public'));
 app.use(morgan('dev'));
